313309102ef7f481f8ba97adb46f120a458633f7
Bas van der Hoek <bas@hoek.com>
2020-06-17 11:24:18 +0200

    colorize kanji from command-line

--- colorize.js
+++ colorize.js
@@ -1,4 +1,17 @@
 var fs = require('fs');
+
+if (process.argv.length !== 3) {
+  console.error(`Usage: colorize 客`)
+  process.exit(1)
+}
+let unicode = process.argv[2].charCodeAt(0).toString(16)
+let source = `/Users/bas/github/kanjivg/kanji/0${unicode}.svg`
+
+if(!fs.existsSync(source)) {
+  console.error(source, "does not exist")
+  process.exit(2)
+}
+
 let colors = [
   "E8ECFB", "D9CCE3", "CAACCB", "BA8DB4", "AA6F9E", "994F88", "882E72", "7BAFDE",
   "6195CF", "437DBF", "1965B0", "CAE0AB", "4EB265", "90C987", "F7F056", "F7CB45",
@@ -32,7 +45,7 @@ saxStream.on('opentag', function (node) {
         text {
         font: 8pt sans-serif;
         stroke-width: 0pt;
-        fill: #839496;
+        fill: #586e75;
         }
         path {
         stroke-width: 4pt;
@@ -56,7 +69,8 @@ saxStream.on('text', function (text) {
 
 let pipes = new Promise(((resolve, reject) => {
   destination.on('finish', resolve)
-  fs.createReadStream('/Users/bas/github/kanjivg/kanji/0f9a8.svg')
+  fs.createReadStream(source)
     .pipe(saxStream)
 }))
+
 Promise.all([pipes]).then(console.log('completed'))
