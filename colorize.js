5c2a8d09592160a319dca3fb456eef9a2cd410f7
Bas van der Hoek <bas@hoek.com>
2020-06-17 11:08:30 +0200

    colorize with sax parser

--- colorize.js
+++ colorize.js
@@ -1,20 +1,62 @@
-var fs = require('fs'),
-  xml2js = require('xml2js');
-var builder = new xml2js.Builder();
-var parser = new xml2js.Parser();
+var fs = require('fs');
+let colors = [
+  "E8ECFB", "D9CCE3", "CAACCB", "BA8DB4", "AA6F9E", "994F88", "882E72", "7BAFDE",
+  "6195CF", "437DBF", "1965B0", "CAE0AB", "4EB265", "90C987", "F7F056", "F7CB45",
+  "F4A736", "EE8026", "E65518", "DC050C", "A5170E", "72190E", "42150A",]
+let i = 0
 
-function nameToUpperCase(name) {
-  return name.toUpperCase();
-}
+let destination = fs.createWriteStream('test.svg');
 
-const traverse = (result) => {
-  console.dir(result);
-}
+var saxStream = require("sax").createStream(true)
+saxStream.on("error", function (e) {
+  console.error("error!", e)
+  this._parser.error = null
+  this._parser.resume()
+})
 
-fs.readFile('/Users/bas/github/kanjivg/kanji/0f9a8.svg', (err, data) => {
-  parser.parseString(data, (err, result) => {
-    traverse(result)
-    var xml = builder.buildObject(result);
-    console.log(xml);
-  });
+saxStream.on('opentag', function (node) {
+  destination.write(`<${node.name}`)
+  if (node.name === 'path') {
+    destination.write(` style="stroke: #${colors[i++ % colors.length]}"`)
+  }
+
+  for (const [key, value] of Object.entries(node.attributes)) {
+    if (!key.startsWith('kvg:') && key !== 'style') {
+      destination.write(` ${key}="${value}"`)
+    }
+  }
+  destination.write(`>`)
+  if (node.name === 'svg') {
+    destination.write(`
+      <style type="text/css">
+        text {
+        font: 8pt sans-serif;
+        stroke-width: 0pt;
+        fill: #839496;
+        }
+        path {
+        stroke-width: 4pt;
+        fill: #ffffff;
+        fill-opacity: 0;
+        }
+      </style>
+    `)
+  }
 });
+
+saxStream.on('closetag', function (node) {
+  destination.write(`</${node}>`)
+});
+
+saxStream.on('text', function (text) {
+  if (text !== '\n') {
+    destination.write(text)
+  }
+});
+
+let pipes = new Promise(((resolve, reject) => {
+  destination.on('finish', resolve)
+  fs.createReadStream('/Users/bas/github/kanjivg/kanji/0f9a8.svg')
+    .pipe(saxStream)
+}))
+Promise.all([pipes]).then(console.log('completed'))
