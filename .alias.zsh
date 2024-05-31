eed72b47b77eb78f8262bede9e588e94b279405a
Bas van der Hoek <bas@hoek.com>
2024-05-31 11:28:53 +0200

    Sort cards in numbered sequence

--- .alias.zsh
+++ .alias.zsh
@@ -5,15 +5,22 @@ anki_sort() {
     return
   fi
 
-  ./anki.js move "$1 card:ToExpress" Japans::Zeggen
-  ./anki.js move "$1 card:ToMeaning" Japans::Lezen
-  ./anki.js move "$1 card:ToKanji" Japans::Schrijven::Kanji
-  ./anki.js move "$1 note:OnKanji card:ToOnYomi" Japans::Spreken::Kanji
-  ./anki.js move "$1 card:ToOnYomi" Japans::Spreken::Jukugo
-  ./anki.js move "$1 card:ToKunYomi" Japans::Spreken::Kunyomi
+  ./anki.js move "$1 card:ToKanji" Japans::1-Schrijven::Kanji
+  ./anki.js move "$1 note:Opposite card:Read*" Japans::1-Schrijven::Opposite
+  ./anki.js move "$1 note:OnKanji card:ToOnYomi" Japans::2-Spreken::Kanji
+  ./anki.js move "$1 card:ToOnYomi" Japans::2-Spreken::Jukugo
+  ./anki.js move "$1 card:ToKunYomi" Japans::2-Spreken::Kunyomi
+  ./anki.js move "$1 card:ToExpress" Japans::3-Zeggen
+  ./anki.js move "$1 card:ToMeaning" Japans::4-Lezen
+  ./anki.js move "$1 note:Opposite card:Listen*" Japans::5-Luisteren::Opposite
 }
 
 anki_process() {
+  if [ $# -lt 1 ]
+  then
+    echo "Usage: anki_process <query>"
+    return
+  fi
   ./anki.js kana $1
   ./anki.js stroke $1
   ./anki.js speech $1
