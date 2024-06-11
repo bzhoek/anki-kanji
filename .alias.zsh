c49b98333608b61bc46ea93aa66b0c8db448142d
Bas van der Hoek <bas@hoek.com>
2024-06-11 09:17:47 +0200

    Sort speaking and listening cards

--- .alias.zsh
+++ .alias.zsh
@@ -10,9 +10,11 @@ anki_sort() {
   ./anki.js move "$1 note:OnKanji card:ToOnYomi" Japans::2-Spreken::Kanji
   ./anki.js move "$1 card:ToOnYomi" Japans::2-Spreken::Jukugo
   ./anki.js move "$1 card:ToKunYomi" Japans::2-Spreken::Kunyomi
+  ./anki.js move "$1 card:Speaking" Japans::2-Spreken
   ./anki.js move "$1 card:ToExpress" Japans::3-Zeggen
   ./anki.js move "$1 card:ToMeaning" Japans::4-Lezen
   ./anki.js move "$1 note:Opposite card:Listen*" Japans::5-Luisteren::Opposite
+  ./anki.js move "$1 card:Listening" Japans::5-Luisteren
 }
 
 anki_process() {
