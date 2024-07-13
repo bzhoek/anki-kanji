d68db9c75d035c89566ebcf4fae870b3d22c7a78
Bas van der Hoek <bas@hoek.com>
2024-07-13 21:17:05 +0200

    Simplify sorting

--- .alias.zsh
+++ .alias.zsh
@@ -8,14 +8,14 @@ anki_sort() {
   ./anki.js move "$1 card:ToKanji" Japans::1-書く::漢字
   ./anki.js move "$1 note:Opposite card:Read*" Japans::1-書く::対義語
   ./anki.js move "$1 note:OnKanji card:ToOnYomi" Japans::2-言う::漢字
-  ./anki.js move "$1 card:ToOnYomi" Japans::2-言う::熟語
-  ./anki.js move "$1 card:ToKunYomi" Japans::2-言う::訓読み
+  ./anki.js move "$1 card:ToOnYomi" Japans::2-言う::単語
+  ./anki.js move "$1 card:ToKunYomi" Japans::2-言う::単語
   ./anki.js move "$1 card:Speaking" Japans::2-言う
   ./anki.js move "$1 card:ToExpress" Japans::3-言う
   ./anki.js move "$1 card:ToMeaning" Japans::4-読む
   ./anki.js move "$1 note:Opposite card:Listen*" Japans::5-聞く::対義語
   ./anki.js move "$1 card:Listening" Japans::5-聞く
-  ./anki.js move "$1 card:ToHearing" Japans::5-聞く
+  ./anki.js move "$1 card:ToHearing" Japans::5-聞く::単語
 }
 
 anki_process() {
