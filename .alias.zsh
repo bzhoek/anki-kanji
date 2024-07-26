095ae66ae84fa5d05f5b2565667677d942920895
Bas van der Hoek <bas@hoek.com>
2024-07-26 13:15:09 +0200

    Share usage prompt and condense decks

--- .alias.zsh
+++ .alias.zsh
@@ -1,9 +1,13 @@
-anki_sort() {
-  if [ $# -lt 1 ]
+usage() {
+  if [ $1 -lt 1 ]
   then
-    echo "Usage: anki_sort <query>"
-    return
+    echo "Usage: ${funcstack[-1]} <query>"
+    return 1
   fi
+}
+
+anki_sort() {
+  usage $# || return
 
   ./anki.js move "$1 card:ToKanji" Japans::1-書く::漢字
   ./anki.js move "$1 note:Opposite card:Read*" Japans::1-書く::対義語
@@ -11,22 +15,23 @@ anki_sort() {
   ./anki.js move "$1 card:ToOnYomi" Japans::2-言う::単語
   ./anki.js move "$1 card:ToKunYomi" Japans::2-言う::単語
   ./anki.js move "$1 card:Speaking" Japans::2-言う
-  ./anki.js move "$1 card:ToExpress" Japans::3-言う
-  ./anki.js move "$1 card:ToMeaning" Japans::4-読む
-  ./anki.js move "$1 note:Opposite card:Listen*" Japans::5-聞く::対義語
-  ./anki.js move "$1 card:Listening" Japans::5-聞く
-  ./anki.js move "$1 card:ToHearing" Japans::5-聞く::単語
+  ./anki.js move "$1 card:ToExpress" Japans::2-言う
+  ./anki.js move "$1 card:ToMeaning" Japans::3-読む
+  ./anki.js move "$1 note:Opposite card:Listen*" Japans::4-聞く::対義語
+  ./anki.js move "$1 card:Listening" Japans::4-聞く
+  ./anki.js move "$1 card:ToHearing" Japans::4-聞く::単語
 }
 
 anki_process() {
-  if [ $# -lt 1 ]
-  then
-    echo "Usage: anki_process <query>"
-    return
-  fi
+  usage $# || return
+
   ./anki.js kana $1
   ./anki.js stroke $1
   ./anki.js speech $1
+}
+
+anki_stroke() {
+  ./anki.js stroke 'kanji:_* strokes:'
 }
 
 anki_default() {
