ca32b619f7634d1839662c6ef456ef5cc6e592bc
Bas van der Hoek <bas@hoek.com>
2025-05-25 16:47:54 +0200

    Add fish sort and process functions

---
+++ .alias.fish
@@ -0,0 +1,32 @@
+function usage
+  set -l actual (count $argv[3..-1])
+  if test $actual -lt $argv[1]
+    echo "Usage: $argv[2]"
+    return 1
+  end
+end
+
+function akprocess
+  usage 1 "Usage: akprocess <query>" $argv || return
+  set QRY $argv[1]
+
+  ./anki.js kana $QRY
+  ./anki.js stroke $QRY
+  ./anki.js furigana $QRY
+  ./anki.js speech $QRY
+  ./anki.js mirror $QRY
+end
+
+function aksort
+  usage 1 "Usage: aksort <query>" $argv || return
+  set FROM $argv[1]
+  
+  ./anki.js move "$FROM (card:ToKanji or card:ToWriting)" Japans::1-書く::漢字
+  ./anki.js move "$FROM note:Opposite card:Read*" Japans::1-書く::対義語
+  ./anki.js move "$FROM card:*Yomi" Japans::2-言う読む
+  ./anki.js move "$FROM (card:Speaking or card:ToExpress or card:ToMeaning)" Japans::2-言う読む
+  ./anki.js move "$FROM note:Opposite card:Listen*" Japans::3-聞く::対義語
+  ./anki.js move "$FROM card:Listening" Japans::3-聞く::没入
+  ./anki.js move "$FROM card:ToHearing" Japans::3-聞く::単語
+  ./anki.js move "$FROM card:Cloze*" Japans::4-文法
+end
